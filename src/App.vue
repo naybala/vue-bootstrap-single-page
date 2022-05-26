d1489c3c2e1cab610de118330a594b1b63c31b15
naybala <naybalaismylife.nbl@gmail.com>
2022-05-26 18:26:33 +0630

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <Navbar />
   <ShowCase />
   <SignUp />
+  <Box />
+  <Footer />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import ShowCase from "./components/ShowCase.vue";
 import SignUp from "./components/SignUp.vue";
+import Box from "./components/Box.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Navbar,
     ShowCase,
     SignUp,
+    Box,
+    Footer,
   },
 };
 </script>
