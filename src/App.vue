475f0334294404f1cf9efccae7da39c59b971048
naybala <naybalaismylife.nbl@gmail.com>
2022-05-26 17:12:31 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+body::before {
+  display: block;
+  content: "";
+  height: 60px;
+}
+
+#map {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+@media (min-width: 768px) {
+  .news-input {
+    width: 50%;
+  }
+}
+</style>
